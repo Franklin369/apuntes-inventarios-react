632c84324a35d0f2c64d3b57f0cad4beb7680084
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2023-11-13 15:55:18 -0500

    Create dependencias.jsx

---
+++ dependencias.jsx
@@ -0,0 +1,21 @@
+npm i react-icons
+npm i styled-components
+npm i react-hook-form
+npm i react-router-dom
+npm i sweetalert2
+npm i zustand
+npm i @tanstack/react-query
+npm i @tanstack/react-table
+npm i @tanstack/react-query-devtools
+npm i @supabase/supabase-js
+npm i react-spinners
+npm i react-lottie
+npm i react-color
+
+
+
+
+
+
+
+
