b9d7a9a8b93bcc717268429b7ceeca752aecc892
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2023-11-13 15:37:50 -0500

    Create breackpoints.jsx

---
+++ breackpoints.jsx
@@ -0,0 +1,12 @@
+const sizes = {
+  mobile: "576px",
+  tablet: "768px",
+  laptop: "992px",
+  desktop: "1200px",
+};
+export const Device = {
+  mobile: `(min-width: ${sizes.mobile})`,
+  tablet: `(min-width: ${sizes.tablet})`,
+  laptop: `(min-width: ${sizes.laptop})`,
+  desktop: `(min-width: ${sizes.desktop})`,
+};
