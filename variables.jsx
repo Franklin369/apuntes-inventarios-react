3afbb3c38062c74bda2fd99bb968acea89629334
codigo369.com <64455345+Franklin369@users.noreply.github.com>
2023-11-13 15:36:46 -0500

    Create variables.jsx

---
+++ variables.jsx
@@ -0,0 +1,112 @@
+import { CiPalette } from "react-icons/ci";
+import { BsEmojiLaughing } from "react-icons/bs";
+import { RiDeleteBin2Line, RiEditLine } from "react-icons/ri";
+import { IoIosArrowDown } from "react-icons/io";
+import { RiVipCrownFill } from "react-icons/ri";
+import { BiUserCircle } from "react-icons/bi";
+import { RiSettings3Line } from "react-icons/ri";
+import { MdExitToApp } from "react-icons/md";
+import { FcPicture } from "react-icons/fc";
+import { CgMathPlus } from "react-icons/cg";
+import { TbBrandSupabase } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { BsArrowDown, BsArrowUpShort } from "react-icons/bs";
+import { FaBalanceScale } from "react-icons/fa";
+import logo from "../assets/inventarioslogo.png";
+import { BsGoogle } from "react-icons/bs";
+import { RiCloseLine } from "react-icons/ri";
+import { BiSave } from "react-icons/bi";
+import { BsQuestionCircle } from "react-icons/bs";
+import { HiOutlineChartPie } from "react-icons/hi";
+import { SlGraph } from "react-icons/sl";
+import { BsBarChartLine } from "react-icons/bs";
+import { AiOutlineCalculator } from "react-icons/ai";
+import { BsCalendarCheck } from "react-icons/bs";
+import {IoIosArrowForward} from "react-icons/io"
+import fondo from "../assets/fondo1.svg";
+import {GrFormPrevious,GrCaretNext} from "react-icons/gr"
+import {MdOutlineBorderAll} from "react-icons/md"
+import { MdAlternateEmail } from "react-icons/md";
+import {RiLockPasswordLine} from "react-icons/ri"
+import {MdDriveFileRenameOutline} from "react-icons/md"
+import {RiStockLine} from "react-icons/ri"
+import {BiBellMinus} from "react-icons/bi"
+import {AiOutlineBarcode} from "react-icons/ai"
+import {DiCodepen} from "react-icons/di"
+import {FaRegMoneyBillAlt} from "react-icons/fa"
+import {CiMoneyBill} from "react-icons/ci"
+import {TbBrandBitbucket} from "react-icons/tb"
+import {MdOutlineCategory} from "react-icons/md"
+import {FaBuilding} from "react-icons/fa"
+export const v = {
+  iconoempresa:FaBuilding,
+  iconocategorias:MdOutlineCategory,
+  iconomarca:TbBrandBitbucket,
+  iconoprecioventa:FaRegMoneyBillAlt,
+  iconopreciocompra:CiMoneyBill,
+  iconocodigointerno:DiCodepen,
+  iconocodigobarras:AiOutlineBarcode,
+  iconostockminimo:BiBellMinus,
+  iconostock:RiStockLine,
+  icononombre:MdDriveFileRenameOutline,
+  iconoemail:MdAlternateEmail,
+  iconopass:RiLockPasswordLine,
+  sidebarWidth: `300px`,
+  sidebarWidthInitial: `10vw`,
+  smSpacing: `8px`,
+  mdSpacing: `16px`,
+  lgSpacing: `24px`,
+  xlSpacing: `32px`,
+  xxlSpacing: `48px`,
+  borderRadius: `6px`,
+
+  paletacolores: CiPalette,
+  emoji: BsEmojiLaughing,
+  iconeditarTabla: RiEditLine,
+  iconeliminarTabla: RiDeleteBin2Line,
+  colorIngresos: `#53B257`,
+  colorbgingresos: `#e6ffe7`,
+  colorGastos: `#fe6156`,
+  colorbgGastos: `#fbcbc9`,
+  colorError: `#F54E41`,
+  colorExito: `#9046FF`,
+  bpmaggie: `15em`,
+  bplisa: `30em`,
+  bpbart: `48em`,
+  bpmarge: `62em`,
+  bphomer: `75em`,
+  colorPrincipal: `#00F34A`,
+  boxshadowGray: `0px 10px 15px -3px rgba(0,0,0,0.1)`,
+  iconoFlechabajo: IoIosArrowDown,
+  iconocorona: RiVipCrownFill,
+  iconoUser: BiUserCircle,
+  iconoSettings: RiSettings3Line,
+  iconoCerrarSesion: MdExitToApp,
+  iconofotovacia: FcPicture,
+  verde: `#53B257`,
+  rojo: `#F54E41`,
+  agregar: CgMathPlus,
+  iconosupabase: TbBrandSupabase,
+  iconoreact: FaReact,
+  flechaabajolarga: BsArrowDown,
+  flechaarribalarga: BsArrowUpShort,
+  balance: FaBalanceScale,
+  logo: logo,
+  iconogoogle: BsGoogle,
+  iconocerrar: RiCloseLine,
+  iconoguardar: BiSave,
+  iconoayuda: BsQuestionCircle,
+  iconopie: HiOutlineChartPie,
+  iconolineal: SlGraph,
+  iconobars: BsBarChartLine,
+  iconocalculadora: AiOutlineCalculator,
+  iconocheck: BsCalendarCheck,
+  imagenfondo: fondo,
+  colorSecundario: `#DAC1FF`,
+  iconoflechaderecha:IoIosArrowForward,
+  colorselector:"#BF94FF",
+  rgbafondos:"rgba(210, 110, 249, 0.1)",
+  iconoprev:GrFormPrevious,
+  icononext:GrCaretNext,
+  iconotodos:MdOutlineBorderAll
+};
